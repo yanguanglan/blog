fbc72486d3ef10c0376fb795baf75ab1809185e6
xngiser <ggiiss@qq.com>
2014-05-07 17:40:59 +0800

    undate

--- codes/xhrLoadJS.js
+++ codes/xhrLoadJS.js
@@ -1,12 +1,13 @@
 function xhrLoadJS (url, callback){
-  var xhr = new XMLhttpRequest();
+  var xhr = new XMLHttpRequest();
   xhr.open('get', url, true);
   xhr.onreadystatechange = function(){
-    if(xhr.ready == 4){
-      if(xhr.status >= 200 && xhr.status < 300 || xhr.status = 304){
+    if(xhr.readyState == 4){
+      if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
         var script = document.createElement('script');
         script.type = 'text/script';
         script.text = xhr.responseText;
+        eval(xhr.responseText);
         document.body.appendChild(script);
         callback();
       }
